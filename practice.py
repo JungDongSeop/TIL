7f62dd75f87ec39058424b512295d0d00a39d91c
JungDongSeop <ds123911@naver.com>
2022-09-16 17:57:32 +0900

    algorithm 2

--- practice.py
+++ practice.py
@@ -1,116 +1,67 @@
-N = int(input())
-arr = list(map(int, input().split()))
+import sys
+sys.stdin = open('input.txt')
 
-def sum_of_stack(stack, answer):
-    tmp = 0
-    for i in stack:
-        tmp += arr[i-1]
-    return min(answer, abs(all_sum - 2*tmp))
+# 백준 12728번
 
-def check():
-    # 시작점 1인 BFS
-    queue = [1]
-    visited = [False] * (N+1)
-    check_set = set([1])
-    while queue:
-        now = queue.pop(0)
-        if not visited[now]:
-            visited[now] = True
-            for next in adjList[now]:
-                queue.append(next)
+# 2**30 < 2000000000 < 2**31
 
-        for i in queue:
-            check_set.add(i)
-    # 이 때 전부 연결했으면, check 완료
-    if len(check_set) == N:
-        return True
+# N을 이진법으로 하고, 각 숫자에 대해 함수 실행
+print(bin(7))
+print(len(bin(7)))
 
-    # 전부 연결 못했으면, 연결 안 한 tmp_start 찾아서 다시 BFS
-    tmp_start = 0
-    for j in range(1, N+1):
-        if j not in check_set:
-            tmp_start = j
-            break
+def func(N):
+    # N이 1이면
+    if N == 1:
+        return 6
+    if N == 2:
+        return 28
+    if N == 3:
+        return 144
 
-    # 다시 BFS
-    queue = [tmp_start]
-    visited = [False] * (N + 1)
-    visited[tmp_start] = True
-    check_set_2 = set()
-    while queue:
-        now = queue.pop(0)
-        for next in adjList[now]:
-            if not visited[next]:
-                queue.append(next)
-                visited[next] = True
-        for i in queue:
-            check_set_2.add(i)
+    howlong = len(bin(N)) - 3
 
-    if len(check_set.union(check_set_2)) == N:
-        return True
+    
+    r = 2**howlong
+    return power_list[howlong] 
+        
+        
 
-    return False
+    pass
 
-def check_reaminder(stack):
-    tmp_start = 0
-    for j in range(1, N + 1):
-        if j not in stack:
-            tmp_start = j
-            break
-    short_adjList = []
-
-    queue = [tmp_start]
-    visited = [False] * (N + 1)
-    check_set = set([1])
-    while queue:
-        now = queue.pop(0)
-        if not visited[now]:
-            visited[now] = True
-            for next in adjList[now]:
-                queue.append(next)
-
-        for i in queue:
-            check_set.add(i)
-    # 이 때 전부 연결했으면, check 완료
-
-def DFS():
-
-    if not check():
-        return -1
-
-    answer = 10000
-    for qq in range(1, N+1):
-        stack = [qq]
-        visited = [False] * (N+1)
-        visited[qq] = True
-
-        while stack:
-            now = stack[-1]
-            if adjList != []:
-                for next in adjList[now]:
-                    if not visited[next]:
-                        stack.append(next)
-                        visited[next] = True
-                        break
-                else:
-                    stack.pop()
-            else:
-                return -1
+    # N이 짝수면
+    if N % 2 == 0:
+        return (1000 + func(N//2) ** 2 - 2 * four_list[((N//2 % 50) + 48) % 50]) % 1000
+    # N이 홀수면
+    # else:
+    #
+    #     return (1000 + power_list[] - 4*func(N-2)) % 1000
 
 
-
-            answer = sum_of_stack(stack, answer)
-
-    return answer
+# 4의 거듭제곱을 1000으로 나눈 나머지는 4^2과 4^52 이 같다.
+four_list = [16, 64, 256, 24, 96, 384, 536, 144, 576, 304, 216, 864, 456, 824, 296, 184, 736, 944, 776, 104, 416, 664, 656, 624, 496, 984, 936, 744, 976, 904, 616, 464, 856, 424, 696, 784, 136, 544, 176, 704, 816, 264, 56, 224, 896, 584, 336, 344, 376, 504]
 
 
+# A^n + B^n 에서 n==2**i 일 때의 값은 power_list[i] 와 같다.
+power_list = [1, 6, 28, 752]
+ab_list = [1, 4, 16, 256]
 
-adjList = [0] * (N+1)
-for q in range(1, N+1):
-    tmp_list = list(map(int, input().split()))
-    tmp_list.pop(0)
-    adjList[q] = tmp_list
+for i in range(4, 32):
+    ab_list.append((ab_list[i-1] ** 2) % 1000)
+    power_list.append((power_list[i-1] ** 2 - 2 * ab_list[i] ** 2) % 1000)
 
-all_sum = sum(arr)
-# print(adjList)
-print(DFS())
+# power_list = [1, 6, 28, 752, 912, 512, 232, 952, 112, 112, 232, 152, 312, 712, 232, 352, 512, 312, 232, 552, 712, 912, 232, 752, 912, 512, 232, 952, 112, 112, 232, 152]
+# ab_list = [1, 4, 16, 256, 536, 296, 616, 456, 936, 96, 216, 656, 336, 896, 816, 856, 736, 696, 416, 56, 136, 496, 16, 256, 536, 296, 616, 456, 936, 96, 216, 656]
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    # tmp = (func(N) + 999) % 1000
+    #
+    # if tmp < 10:
+    #     answer = '00' + str(tmp)
+    # elif tmp < 100:
+    #     answer = '0' + str(tmp)
+    # else:
+    #     answer = str(tmp)
+    # print(f'Case #{tc}: {answer}')
+
